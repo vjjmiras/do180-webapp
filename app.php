27d66a536f09443688ee472cf1df7cff914a9310
Student User <student@workstation.lab.example.com>
2020-05-28 09:52:57 +0000

    stage 2 from application

--- app.php
+++ app.php
@@ -4,8 +4,17 @@
   </head>
   <body>
     <?php
-      $quote = "Der frühe Vogel fängt den Wurm.";
-      echo $quote . "<br/>\n";
+      // v2
+      $quotes = array(
+        "Der frühe Vogel fängt den Wurm.",
+        "Pech im Spiel, Glück in der Liebe.",
+        "Keine Antwort ist auch eine Antwort.",
+        "Der Hunger kommt beim Essen.",
+        "Es ist leichter sich zu entschuldigen als vorher um Erlaubnis zu fragen.",
+        "Fliegen und Freunde kommen im Sommer."
+      );
+      // end of v2
+      echo $quotes[rand(0,count($quotes)-1)] . "<br/>\n";
     ?>
   </body>
 </html>
