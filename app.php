dd75578bdfb34830a9947fc7fe19017bb7559385
Student User <student@workstation.lab.example.com>
2020-05-28 09:06:01 +0000

    initial commit

---
+++ app.php
@@ -0,0 +1,11 @@
+<html>
+  </head>
+    <title>Generating a quote</title>
+  </head>
+  <body>
+    <?php
+      $quote = "Der frühe Vogel fängt den Wurm.";
+      echo $quote . "<br/>\n";
+    ?>
+  </body>
+</html>
